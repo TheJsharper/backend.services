8b7380673890b6ef21b4666c4923832a5cbf417f
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-08-23 00:14:39 +0200

    entities, repositories added

--- order-service/src/main/java/com/jsharper/orderservice/repositories/PurchaseOrderRepository.java
+++ order-service/src/main/java/com/jsharper/orderservice/repositories/PurchaseOrderRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jsharper.orderservice.entities.PurchaseOrder;
 
-public interface PurchaseorderRepository extends JpaRepository<PurchaseOrder, Integer> {
+public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Integer> {
 
 }
