fd4be514df7eb1f94ebff8eb7ce0b20ac38335de
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-08-23 00:14:06 +0200

    entities, repositories added

---
+++ order-service/src/main/java/com/jsharper/orderservice/entities/PurchaseOrder.java
@@ -0,0 +1,28 @@
+package com.jsharper.orderservice.entities;
+
+import org.springframework.data.annotation.Id;
+
+import com.jsharper.orderservice.dtos.OrderStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Getter
+@Setter
+@ToString
+@Entity
+public class PurchaseOrder {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String productId;
+	private Integer userId;
+	private Integer amount;
+	private OrderStatus status;
+}
---
+++ order-service/src/main/java/com/jsharper/orderservice/repositories/PurchaseOrderRepository.java
@@ -0,0 +1,9 @@
+package com.jsharper.orderservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsharper.orderservice.entities.PurchaseOrder;
+
+public interface PurchaseorderRepository extends JpaRepository<PurchaseOrder, Integer> {
+
+}
